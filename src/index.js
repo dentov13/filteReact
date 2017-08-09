afede279d15af4c860051b612362273449f3b6a7
dentov13 <tovstokhatko.denis@gmail.com>
2017-08-09 16:27:55 +0300

    change logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 import '../node_modules/material-components-web/dist/material-components-web.min.css'
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  state = {
+    icon: '',
+    time: 1,
+    city: '',
+    temperature: '',
+    weatherCode: '',
+    fetching: true
+  }
+
+  componentDidMount() {
+    this.fetchIP();
+  }
+
+  fetchWeatherData = city => {
+    const baseUrl = `http://api.openweathermap.org`;
+    const path = `/data/2.5/weather`;
+    const appId = `459fc1107972f9ca8e39abe5f2583722`;
+    const query = `units=metric&lang=ru&appid=${appid}`;
+  }
+}
